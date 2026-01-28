956a4bda4a8a279cea2db1b0a8e77869dbd2c045
srabhayraj <sr.abhayraj@gmail.com>
2026-01-28 12:20:20 +0530

    project-1 added for report aws resource usage

---
+++ Project-1_AWS-resource-usage/aws_resource_tracker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+##################################################
+#
+# Author: Abhay Raj Singh Rathore
+# Date: 28th Jan 2026
+# Version: v1
+#
+# This script will report the AWS resource usage.
+#
+# ################################################
+#
+set -x
+#
+# Resources that will be tracked:
+# AWS S3
+# AWS EC2
+# AWS IAM
+# AWS Lambda
+#
+# List S3 buckets
+echo "Print list of S3 buckets"
+aws s3 ls
+
+#List ECC2 instances
+echo "Print list of EC2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+#jq command is used to parse JSON output, and [] is used to iterate through arrays as Reservations and Instances are arrays.
+
+#List IAM users
+echo "Print list of IAM users"
+aws iam list-users
+
+#List Lambda functions
+echo "Print List of Lambda functions"
+aws lambda list-functions
